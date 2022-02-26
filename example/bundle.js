2e506666afa36a3d90774b1b3cc5e4b81ed9887d
qinran0423 <1156107187@qq.com>
2022-02-26 20:59:27 +0800

    feat: 实现build 打包

--- example/bundle.js
+++ example/bundle.js
@@ -1,32 +1,39 @@
 
 
 (function (modules) {
-  function require(filePath) {
-    const fn = modules[filePath]
+  function require(id) {
+    const [fn, mapping] = modules[id]
 
     const module = {
       exports: {}
     }
-    fn(require, module, module.exports)
+
+    function localRequire(filePath) {
+      const id = mapping[filePath]
+      return require(id)
+    }
+
+
+    fn(localRequire, module, module.exports)
 
     return module.exports
   }
 
-  require('./main.js')
+  require(1)
 
 })({
-  './foo.js': function (require, module, exports) {
+  1: [function (require, module, exports) {
+    const { foo } = require('./foo.js')
+    foo()
+    console.log('main.js');
+  }, {'./foo.js': 2}],
+  2: [function (require, module, exports) {
     function foo() {
       console.log('foo');
     }
     module.exports = {
       foo
     }
-  },
-  './main.js': function (require, module, exports) {
-    const { foo } = require('./foo.js')
-    foo()
-    console.log('main.js');
-  }
+  },{}]
 
 });
--- index.js
+++ index.js
@@ -4,8 +4,7 @@ import ejs from 'ejs'
 import parser from '@babel/parser'
 import traverse from "@babel/traverse";
 import { transformFromAst } from 'babel-core';
-
-
+let id = 0
 function createAsset(filePath) {
 
   // 1. 获取文件内容
@@ -19,30 +18,38 @@ function createAsset(filePath) {
   })
 
   const deps = []
+  // traverse解析获取依赖模块的路径地址 然后存入到deps中
   traverse.default(ast, {
     ImportDeclaration({ node }) {
+      console.log('node', node);
       deps.push(node.source.value)
     }
   })
-
+  // 将esmodule 转换成 commomjs
   const { code } = transformFromAst(ast, null, {
     presets: ['env']
   })
   return {
     filePath,
     code,
-    deps
+    deps,
+    mapping: {}, // 存储当前模块的依赖模块的id
+    id: id++ // 每个模块都有一个唯一的id
   }
 }
 
 //构建图
 function createGraph() {
+  // 获取入口文件的文件内容 以及 依赖关系图
   const mainAsset = createAsset('./example/main.js')
-
+  // 将各个文件对应的依赖关系 收集起来
   const queue = [mainAsset]
   for (const asset of queue) {
     asset.deps.forEach(relativePath => {
+      // 遍历解析依赖的模块
       const child = createAsset(path.resolve('./example', relativePath))
+      // 将当前依赖的模块的唯一id存储起来
+      asset.mapping[relativePath] = child.id
       queue.push(child)
     });
   }
@@ -55,12 +62,15 @@ const graph = createGraph()
 
 
 function build() {
+  // 读取ejs模板文件  生成模板
   const template = fs.readFileSync('./bundle.ejs', { encoding: 'utf-8' })
-
+  
   const data = graph.map((asset) => {
+    const {id, code, mapping} = asset
     return { 
-      filePath: asset.filePath,
-      code: asset.code
+      id,
+      code,
+      mapping
     }
   })
   console.log(data);
